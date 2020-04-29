b37c8fd48223ad2ae203f1f46678f002a2bd4d5b
ZharikovaES <z.kate2000@gmail.com>
2020-04-30 01:39:37 +0300

    Added navigation menu

--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@ $(function(){
     $('.slider').slick({
         arrows: false,
         fade: true,
-        autoplay: 10000,
+        autoplay: true,
+        autoplaySpeed: 5000,
         dots: true
+    });
+
+    $('.header-btn').on('click', function(){
+        $('.menu').addClass('active');
+    });
+
+    $('.close-btn').on('click', function(){
+        $('.menu').removeClass('active');
     });
 });
